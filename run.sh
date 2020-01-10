f6ac52910146a92ee1b9bda022b32adbde02d15d
xnumtw <xnumtw@gmail.com>
2020-01-10 11:41:14 +0800

    enable O2 and add result

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-g++ threadsafe.cxx -std=c++17 -lbenchmark -pthread -ltbb -lboost_thread -o mybenchmark && ./mybenchmark
+g++ threadsafe.cxx -O2 -std=c++17 -lbenchmark -pthread -ltbb -lboost_thread -o mybenchmark && ./mybenchmark
